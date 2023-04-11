a6f3af4192a154a0121ce857fdb17332f30c0f67
danielcols <dannycols.jr@hotmail.com>
2023-04-10 20:40:00 -0400

    pushing files

--- static/PieLogic.js
+++ static/PieLogic.js
@@ -1,31 +1,30 @@
-// HTML
+// html
 
-// / <!DOCTYPE html>
-// <html>
-//   <head>
-//   <meta charset="UTF-8" />
-//     <title>Mortality Rate</title>
-//     <script src="https://d3js.org/d3.v5.min.js"></script>
-//   </head>
-//   <body>
-//     <div id="pie"></div>
-//     <script src="app.js"></script>
-//   </body>
-// </html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Pie Chart</title>
+    <script src="https://d3js.org/d3.v5.min.js"></script>
+  </head>
+  <body>
+    <div id="pie"></div>
+    <script src="app.js"></script>
+  </body>
+</html>
 
-// data for pie chart
+// array created
 const data = [
   { label: 'Alive', value: 10 },
   { label: 'Dead', value: 20 },
-  { label: 'other', value: 15 },
+  { label: 'Other', value: 15 },
 ];
 
-// Dimensions
+// dimensions and margins
 const width = 400;
 const height = 400;
 const margin = 40;
 
-// SVG element
+// SVG elements
 const svg = d3.select('#chart')
   .append('svg')
   .attr('width', width)
@@ -36,15 +35,15 @@ const svg = d3.select('#chart')
   .attr('y', margin / 2)
   .attr('text-anchor', 'middle')
   .style('font-size', '18px')
-  .text('Mortality Rate');
+  .text('Animal Mortality Rate');
 
-// set up pie layout
-const pie = d3.pie()
+// Pie layout
+  const pie = d3.pie()
   .value(d => d.value);
 
 const pieData = pie(data);
 
-// arc generator function
+// arc generator 
 const radius = Math.min(width, height) / 2 - margin;
 
 const arc = d3.arc()
@@ -52,7 +51,7 @@ const arc = d3.arc()
   .outerRadius(radius);
 
 // create pie chart
-const chart = svg.append('g')
+  const chart = svg.append('g')
   .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
 const arcs = chart.selectAll('arc')
@@ -68,3 +67,7 @@ arcs.append('text')
   .attr('transform', d => `translate(${arc.centroid(d)})`)
   .attr('text-anchor', 'middle')
   .text(d => d.data.label);
+
+// pie color
+var color = d3.scaleOrdinal()
+.range(["#0DFA60", "#F51A00", "#0DB1FA"]);
